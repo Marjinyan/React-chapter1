0c4e72ced9c0032b729ff18b103d4f13bfc3ac63
Marjinyan <102169637+Marjinyan@users.noreply.github.com>
2022-07-20 18:27:47 +0400

    Add files via upload
    Ներկայացված է React.js -ի գրքի,  1.4 ենթագլխում Angular.js -ով մշակված փոքրիկ օրինակը:

---
+++ app.js
@@ -0,0 +1,7 @@
+const app = angular.module("MyProject", [])
+app.controller("Calculator", function($scope){
+    $scope.text = "Let's find the perfect weight for you!"
+    $scope.height = 170
+	$scope.weight = 70
+
+})
